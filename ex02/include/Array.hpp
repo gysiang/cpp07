97ed02df0caf3e574b4dbb5bb534422a7d45c0f9
Goh Yong Siang Ivan <gyong-si@c1r5s5.42singapore.sg>
2025-01-22 13:23:31 +0800

    done ex02

--- ex02/include/Array.hpp
+++ ex02/include/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: gyong-si <gyongsi@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 15:45:21 by gyong-si          #+#    #+#             */
-/*   Updated: 2025/01/22 10:32:20 by gyong-si         ###   ########.fr       */
+/*   Updated: 2025/01/22 13:12:15 by gyong-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include <iostream>
 #include <stdexcept>
+#include <string>
 
 template <typename T>
 
@@ -42,8 +43,112 @@ class Array
 		{
 			public:
 				virtual const char	*what() const throw();
-		}
+		};
 
 };
+
+template <typename T>
+Array<T>::Array() : array(NULL), size(0)
+{
+	std::cout << "Default constructor of size 0 called" << std::endl;
+};
+
+/**
+ * Init a array with size of n
+ * Use new to alloc memory
+ */
+
+template <typename T>
+Array<T>::Array(unsigned int n) : size(n)
+{
+	std::cout << "Default constructor of size " << n << " called" << std::endl;
+	array = new T[size];
+}
+
+/**
+ * Copy constructor
+ * Get the size of src and try to copy the contents into
+ * this new array
+ */
+template <typename T>
+Array<T>::Array(const Array &src) : size(src.size)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	if (src.size > 0)
+	{
+		array = new T[src.size];
+		for (unsigned int i = 0; i < src.size; i++)
+		{
+			array[i] = src.array[i];
+		}
+	}
+	else
+		array = NULL;
+}
+
+/**
+ * Assignment operator
+ *
+ */
+template <typename T>
+Array<T> &Array<T>::operator=(Array const &src)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &src)
+	{
+		delete[] array;
+		size = src.size;
+		if (src.size > 0)
+		{
+			array = new T[src.size];
+			for (unsigned int i = 0; i < src.size; i++)
+			{
+				array[i] = src.array[i];
+			}
+		}
+		else
+			array = NULL;
+	}
+	return *this;
+}
+
+/**
+ * overload operator, overloads the subscript so i can access the item in the array
+ * etc array[2]
+ */
+template <typename T>
+T &Array<T>::operator[](unsigned int index)
+{
+	if (array == NULL || index >= size)
+	{
+		throw (InvalidIndexException());
+	}
+	return (array[index]);
+}
+
+/**
+ * Deconstructor - Should delete the memory allocated
+ */
+template <typename T>
+Array<T>::~Array()
+{
+	delete[] array;
+};
+
+/**
+ * Getter
+ */
+template <typename T>
+unsigned int Array<T>::getSize() const
+{
+	return (size);
+}
+
+template <typename T>
+const char *Array<T>::InvalidIndexException::what() const throw()
+{
+	return ("Error: Index out of range.\n");
+}
+
 
 #endif
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -3,16 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gyong-si <gyongsi@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 15:45:52 by gyong-si          #+#    #+#             */
-/*   Updated: 2025/01/09 15:46:09 by gyong-si         ###   ########.fr       */
+/*   Updated: 2025/01/22 13:21:59 by gyong-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Array.hpp"
+#include <cstdlib>
+#include <sstream>
+
 
 int main()
 {
-	return (0);
+	Array<std::string> array1(5);
+	std::string	*a = new std::string[5];
+
+	//srand(time(NULL));
+	for (int i = 0; i < 5; i++)
+	{
+		//const int val = rand();
+		std::ostringstream oss;
+		oss << (i + 1);
+		std::string val = "String " + oss.str();
+		array1[i] = val;
+		a[i] = val;
+	}
+	for (int i = 0; i < 5; i++)
+		std::cout << "array1[" << i << "]:\t" << array1[i] << std::endl;
+	for (int i = 0; i < 5; i++)
+		std::cout << "a[" << i << "]:\t" << a[i] << std::endl;
+	Array<std::string> tmp;
+	tmp = array1;
+
+	array1[0] = "String modified";
+	std::cout << "After changing: modifying either the original array or its copy after copying must not affect the other array\n";
+	for (int i = 0; i < 5; i++)
+		std::cout << "array1[" << i << "]:\t" << array1[i] << std::endl;
+	for (int j = 0; j < 5; j++)
+		std::cout << "tmp[" << j << "]:\t" << tmp[j] << std::endl;
+	// Test Getsize
+	std::cout << "Test getsize\n";
+	std::cout << "Size of array1: " << array1.getSize() << std::endl;
+
+	delete[] a;
 }
